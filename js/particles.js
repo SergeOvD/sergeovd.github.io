3b7c4424e9c09f37b146c8366106edd9a45d096e
Serhii <sergeyovd@alumni.nottingham.ac.uk>
2018-07-17 12:28:21 +0300

    Update particles.js

--- js/particles.js
+++ js/particles.js
@@ -1,67 +1,68 @@
-particlesJS("particles-js", {
+{
   "particles": {
     "number": {
-      "value": 100,
+      "value": 150,
       "density": {
         "enable": true,
         "value_area": 800
       }
     },
     "color": {
-      "value": "random"
+      "value": "#ffa400"
     },
     "shape": {
       "type": "triangle",
       "stroke": {
-        "width": 0,
+        "width": 1,
         "color": "#000000"
       },
       "polygon": {
-        "nb_sides": 5
+        "nb_sides": 3
       },
       "image": {
+        "src": "img/github.svg",
         "width": 100,
         "height": 100
       }
     },
     "opacity": {
-      "value": 0.90,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0.1,
-        "sync": false
-      }
-    },
-    "size": {
-      "value": 5,
+      "value": 1,
       "random": true,
       "anim": {
         "enable": true,
         "speed": 1,
-        "size_min": 0.1,
+        "opacity_min": 0.2,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 10,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 5,
+        "size_min": 1.5,
         "sync": false
       }
     },
     "line_linked": {
       "enable": true,
-      "distance": 150.37479644244019,
+      "distance": 150,
       "color": "#ffffff",
-      "opacity": 0.7505962670675185,
-      "width": 1.4530665163472378
+      "opacity": 0.4,
+      "width": 1
     },
     "move": {
       "enable": true,
-      "speed": 2.5,
+      "speed": 6,
       "direction": "none",
-      "random": false,
+      "random": true,
       "straight": false,
       "out_mode": "out",
-      "bounce": true,
+      "bounce": false,
       "attract": {
-        "enable": true,
-        "rotateX": 400.851810096455,
+        "enable": false,
+        "rotateX": 600,
         "rotateY": 1200
       }
     }
@@ -81,14 +82,14 @@ particlesJS("particles-js", {
     },
     "modes": {
       "grab": {
-        "distance": 400,
+        "distance": 250,
         "line_linked": {
           "opacity": 1
         }
       },
       "bubble": {
         "distance": 400,
-        "size": 100,
+        "size": 40,
         "duration": 2,
         "opacity": 8,
         "speed": 3
@@ -106,22 +107,4 @@ particlesJS("particles-js", {
     }
   },
   "retina_detect": true
-});
-var count_particles, stats, update;
-stats = new Stats;
-stats.setMode(0);
-stats.domElement.style.position = 'absolute';
-stats.domElement.style.left = '0px';
-stats.domElement.style.top = '0px';
-document.body.appendChild(stats.domElement);
-count_particles = document.querySelector('.js-count-particles');
-update = function() {
-  stats.begin();
-  stats.end();
-  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-  }
-  requestAnimationFrame(update);
-};
-requestAnimationFrame(update);;
-
+}
